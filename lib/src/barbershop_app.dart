48b533141ee1f38e6c59a8da39f0a5ebe4416cfb
Luizzlcs <luizzlcs@gmail.com>
2023-12-22 14:24:57 -0300

    Adicionando rota de login page.

--- lib/src/barbershop_app.dart
+++ lib/src/barbershop_app.dart
@@ -1,18 +1,28 @@
 import 'package:asyncstate/asyncstate.dart';
+import 'package:dw_barbershop/src/core/ui/barbershop_theme.dart';
+import 'package:dw_barbershop/src/features/auth/login/login_page.dart';
 import 'package:dw_barbershop/src/features/splash/splash_page.dart';
 import 'package:flutter/material.dart';
+
+import 'core/ui/widgets/barbershop_loader.dart';
 
 class BarbershopApp extends StatelessWidget {
   const BarbershopApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AsyncStateBuilder(builder: (asyncNavigatorObserver) {
-      return MaterialApp(
-        title: 'DW Barbershop',
-        navigatorObservers: [asyncNavigatorObserver],
-        routes: {'/': (context) => const SplashPage()},
-      );
-    });
+    return AsyncStateBuilder(
+        customLoader: const BarbershopLoader(),
+        builder: (asyncNavigatorObserver) {
+          return MaterialApp(
+            title: 'DW Barbershop',
+            theme: BarbershopTheme.themeData,
+            navigatorObservers: [asyncNavigatorObserver],
+            routes: {
+              '/': (context) => const SplashPage(),
+              '/auth/login': (context) => const LoginPage(),
+            },
+          );
+        });
   }
 }
